76a1397683b15473d1a5d5008d91d80e21ccf3eb
FrozenFOXX <siliconfoxx@gmail.com>
2017-12-23 22:40:52 -0600

    Updated init_style for Ubuntu and Debian
    Added explicit setting of Ubuntu and Debian $init_style. Older than 14.04 Ubuntu was set to `upstart` and older Debian than 8.0 was set to `debian`. If later, both are set to `systemd`.

--- manifests/params.pp
+++ manifests/params.pp
@@ -35,8 +35,18 @@ class hubot::params {
   $install_nodejs       = true
 
   case $::operatingsystem {
-    /Ubuntu|Debian/: {
+    /Debian/: {
       if versioncmp($::operatingsystemrelease, '7.0') > 0 {
+        $init_style         = 'systemd'
+      }
+      else {
+        $init_style         = 'debian'
+        }
+      $hubot_init         = "hubot.init.${::operatingsystem}.erb"
+      $nodejs_manage_repo = true
+    }
+    /Ubuntu/: {
+      if versioncmp($::operatingsystemrelease, '14.04') > 0 {
         $init_style         = 'systemd'
       }
       else {
